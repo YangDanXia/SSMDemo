72bd9496e4c7ea7ce351c2c53c3593ab6403e847
DESKTOP-NURVS3E\YDX <243414588@qq.com>
2019-04-20 15:05:24 +0800

    添加mybatis配置

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -5,8 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class UserController {
@@ -20,5 +23,23 @@ public class UserController {
         user.setUserName("Tom");
         log.info(user.toString());
         return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public Map jsonTest(HttpServletRequest request) {
+        System.out.printf("Successfully \n");
+        String id = request.getParameter("id");
+        String name = request.getParameter("name");
+        Map map = new HashMap();
+        map.put("id",id);
+        map.put("name",name);
+        return map;
     }
 }
