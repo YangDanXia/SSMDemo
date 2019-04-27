026594813347ea4f2640d1782d51d7284d8f847e
DESKTOP-NURVS3E\YDX <243414588@qq.com>
2019-04-27 15:34:14 +0800

    解决跨域请求失败问题： No 'Access-Control-Allow-Origin' header is present on the requested resource.

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -12,8 +12,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
+@ResponseBody
 @Api(tags = "测试接口",description = "测试文档说明")
 public class UserController {
     private static Logger log = LoggerFactory.getLogger(UserController.class);
@@ -24,11 +32,14 @@ public class UserController {
     @ApiOperation(value = "查询id为1的人员")
     @ApiImplicitParam(name = "userName",value="姓名",dataType="String",paramType = "query")
     @ApiResponse(response = UserController.class,code=200,message = "返回对象参数")
-    public String test() {
+    public void test(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.printf("Successfully 2019 \n");
         User user = userService.select(1);
         System.out.println(user.toString());
-        return "hello";
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("Hello " + request.getParameter("name"));
+        out.flush();
     }
 
 
